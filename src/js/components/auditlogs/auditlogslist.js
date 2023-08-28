02fd49b2652f01ebe0168119aa87437fce2aca94
Igor Aleksandrychev <16775360+aleksandrychev@users.noreply.github.com>
2023-08-28 13:29:29 +0300

    Update src/js/components/auditlogs/auditlogslist.js
    Co-authored-by: Manuel Zedel <zedel@outlook.com>

--- src/js/components/auditlogs/auditlogslist.js
+++ src/js/components/auditlogs/auditlogslist.js
@@ -26,7 +26,7 @@ import EventDetailsDrawer from './eventdetailsdrawer';
 export const defaultRowsPerPage = 20;
 
 const ViewDetailLink = ({ clickHandler }) => (
-  <a onClick={() => clickHandler()}>
+  <a onClick={clickHandler}>
     View detail <ArrowRightAltIcon />
   </a>
 );
