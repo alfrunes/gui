4ed9ab3d0fcd15af505225a702ae14abb203b788
Manuel Zedel <manuel.zedel@northern.tech>
2023-11-13 16:47:32 +0100

    chore: aligned jwt-decode usage with updated dependency
    Signed-off-by: Manuel Zedel <manuel.zedel@northern.tech>

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -13,7 +13,7 @@
 //    limitations under the License.
 import React from 'react';
 
-import jwtDecode from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 import moment from 'moment';
 import pluralize from 'pluralize';
 
